CityOfBoston/contextual_range_filter

commit 36f554072838d84566af48ff5a6843e8c7f01d8d

array format

src/ContextualRangeFilter.php
@@ -32,7 +32,7 @@ class ContextualRangeFilter {
     if (count($from_to) < 2) {
       $from_to = explode(self::CONTEXTUAL_RANGE_FILTER_SEPARATOR2, $range);
     }
-    return count($from_to) == 1 ? array(reset($from_to), FALSE) : $from_to;
+    return count($from_to) == 1 ? [reset($from_to), FALSE] : $from_to;
   }
 
   /**
@@ -69,7 +69,7 @@ class ContextualRangeFilter {
       if ($to === FALSE) {
         // Dealing with a single value, not a range.
         $operator = $is_not ? '!=' : '=';
-        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", array($placeholder => $range));
+        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", [$placeholder => $range]);
       }
       elseif ($from != '' && $to != '') {
         // From--To.
@@ -77,17 +77,17 @@ class ContextualRangeFilter {
         $placeholder_from = $placeholder;
         $placeholder_to = $views_argument_plugin->query->placeholder($real_field);
         $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder_from AND $placeholder_to $null_check",
-          array($placeholder_from => $from, $placeholder_to => $to));
+          [$placeholder_from => $from, $placeholder_to => $to)];
       }
       elseif ($from != '') {
         // From--.
         $operator = $is_not ? '<' : '>=';
-        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", array($placeholder => $from));
+        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", [$placeholder => $from]);
       }
       elseif ($to != '') {
         // --to.
         $operator = $is_not ? '>' : '<=';
-        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", array($placeholder => $to));
+        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder $null_check", [$placeholder => $to]);
       }
     }
   }

commit 1341d3c9caef1e53a58b55841ff545867bc681cd

Issue #3114613 by ELC: Typo introduced in commit 36f5540

src/ContextualRangeFilter.php
@@ -76,8 +76,9 @@ class ContextualRangeFilter {
         $operator = $is_not ? 'NOT BETWEEN' : 'BETWEEN';
         $placeholder_from = $placeholder;
         $placeholder_to = $views_argument_plugin->query->placeholder($real_field);
-        $views_argument_plugin->query->addWhereExpression($group, "$field $operator $placeholder_from AND $placeholder_to $null_check",
-          [$placeholder_from => $from, $placeholder_to => $to)];
+        $args = [$placeholder_from => $from, $placeholder_to => $to];
+        $expression = "$field $operator $placeholder_from AND $placeholder_to $null_check";
+        $views_argument_plugin->query->addWhereExpression($group, $expression, $args);
       }
       elseif ($from != '') {
         // From--.