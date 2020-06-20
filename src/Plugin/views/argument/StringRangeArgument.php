de5eda2744b32f57114871fab7511db528c79799
RdeBoer <rikdeboer1@yahoo.com.au>
2020-06-20 23:35:39 +1000

    Support + (ie OR) for string ranges

--- src/Plugin/views/argument/StringRangeArgument.php
+++ src/Plugin/views/argument/StringRangeArgument.php
@@ -57,7 +57,7 @@ class StringRangeArgument extends StringArgument {
     }
     // Check "Allow multple ranges" checkbox.
     if (!empty($this->options['break_phrase'])) {
-      $this->breakPhraseString($argument, $this);
+      $this->breakPhraseRange($this->argument);
     }
     else {
       $this->value = [$argument];
@@ -80,6 +80,25 @@ class StringRangeArgument extends StringArgument {
       $field = $this->getFormula();
     }
     ContextualRangeFilter::buildRangeQuery($this, $field);
+  }
+
+  /**
+   * Break xfrom--xto+yfrom--yto+zfrom--zto into an array or ranges.
+   *
+   * @param string $str
+   *   The string to parse.
+   */
+  protected function breakPhraseRange($str) {
+    if (empty($str)) {
+      return;
+    }
+    $this->value = preg_split('/[+ ]/', $str);
+    $this->operator = 'or';
+    // Keep an 'error' value if invalid ranges were given.
+    // A single non-empty value is ok, but a plus sign without values is not.
+    if (count($this->value) > 1 && (empty($this->value[0]) || empty($this->value[1]))) {
+      $this->value = FALSE;
+    }
   }
 
 }
